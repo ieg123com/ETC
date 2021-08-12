7ae0150595df4d655a7dab7cb6b1d624b0f41b11
Ieg123 <154439441@qq.com>
2021-08-12 09:44:35 +0800

    IPEndPoint 添加构造函数

--- model/module/actor/IPEndPoint.h
+++ model/module/actor/IPEndPoint.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <string>
+#include "string/str.h"
 
 
 class IPEndPoint
@@ -12,5 +12,21 @@ public:
 	IPEndPoint() {
 		Ip = "";
 		Port = 0;
+	}
+
+	IPEndPoint(const std::string& ip, const uint32_t port) {
+		Ip = ip;
+		Port = port;
+	}
+
+	IPEndPoint(const std::string& addr) {
+		Ip = "";
+		Port = 0;
+
+		auto strs = std::split(addr, ":");
+		if (strs.size() > 0)
+			Ip = strs[0];
+		if (strs.size() > 1)
+			Port = std::to<uint32_t>(strs[1]);
 	}
 };
