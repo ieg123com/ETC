d21a2bec0935525c05c1184c29f6b862297d374b
Ieg123 <154439441@qq.com>
2021-09-11 09:36:02 +0800

    LocationComponent 功能添加

--- model/module/actorlocation/LocationComponent.cpp
+++ model/module/actorlocation/LocationComponent.cpp
@@ -3,15 +3,18 @@
 
 void LocationComponent::Add(const int64_t key, const int64_t instance_id)
 {
-	m_key_lock[key].lock();
+	auto lock = GetMutex(key);
+	lock->lock();
 	m_key_location[key] = instance_id;
 	LOG_DEBUG("location add key: {}  instance id: {}",key,instance_id);
-	m_key_lock[key].unlock();
+	lock->unlock();
 }
 
 void LocationComponent::Lock(const int64_t key, const int64_t instance_id, const time_t time)
 {
-	m_key_lock[key].lock();
+	auto lock = GetMutex(key);
+	lock->lock();
+	LOG_DEBUG("location lock key: {}  new location id: {}  time: {}", key, instance_id,time);
 	if (time > 0)
 	{
 		co_sleep(time);
@@ -19,17 +22,46 @@ void LocationComponent::Lock(const int64_t key, const int64_t instance_id, const
 	}
 }
 
-void LocationComponent::UnLock(const int64_t key, const int64_t old_instance_id, const int64_t instance_id)
+void LocationComponent::UnLock(const int64_t key, const int64_t old_instance_id, const int64_t new_instance_id)
 {
+	if (m_key_location[key] != old_instance_id)
+		return;
+	LOG_DEBUG("location unlock key: {}  new location id: {}", key, new_instance_id);
+	auto lock = GetMutex(key);
+	m_key_location[key] = new_instance_id;
+	if (lock->is_lock())
+	{
+		lock->unlock();
+	}
 
 }
 
 void LocationComponent::Remove(const int64_t key)
 {
-
+	auto lock = GetMutex(key);
+	lock->lock();
+	m_key_location.erase(key);
+	m_key_lock.erase(key);
+	LOG_DEBUG("location remove key: {}", key);
+	lock->unlock();
 }
 
 int64_t LocationComponent::Get(const int64_t key)
 {
+	auto lock = GetMutex(key);
+	lock->lock();
+	InstanceID instance_id = m_key_location[key];
+	LOG_DEBUG("location get key: {}  location id: {}", key, instance_id);
+	lock->unlock();
+	return instance_id;
+}
 
+std::shared_ptr<co::CoMutex> LocationComponent::GetMutex(const int64_t key)
+{
+	auto found = m_key_lock.find(key);
+	if (found == m_key_lock.end())
+	{
+		return m_key_lock.emplace(key, std::make_shared<co::CoMutex>()).first->second;
+	}
+	return found->second;
 }
--- model/module/actorlocation/LocationComponent.h
+++ model/module/actorlocation/LocationComponent.h
@@ -14,7 +14,7 @@ class LocationComponent :
 	// key: id	value: location id
 	std::unordered_map<int64_t, int64_t>		m_key_location;
 	// 每个 id 的锁
-	std::unordered_map<int64_t, co::CoMutex>	m_key_lock;
+	std::unordered_map<int64_t, std::shared_ptr<co::CoMutex>>	m_key_lock;
 
 public:
 	static LocationComponent* Instance;
@@ -24,11 +24,13 @@ public:
 	// 不是任何一个人就可以上锁的
 	void Lock(const int64_t key, const int64_t instance_id ,const time_t time = 60000);
 
-	void UnLock(const int64_t key, const int64_t old_instance_id, const int64_t instance_id);
+	void UnLock(const int64_t key, const int64_t old_instance_id, const int64_t new_instance_id);
 
 	void Remove(const int64_t key);
 
 	int64_t Get(const int64_t key);
+
+	std::shared_ptr<co::CoMutex> GetMutex(const int64_t key);
 
 
 
